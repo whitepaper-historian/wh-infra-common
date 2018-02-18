6da6e49dc6e6d8136c3c6e21b7d70b14eb1d779a
gcardonag <gcardonagarcia@gmail.com>
2018-02-18 14:07:17 -0500

    Updating PATH in lambci build

--- terraform-build.sh
+++ terraform-build.sh
@@ -5,7 +5,8 @@ cd tmp
 #install terraform
 curl https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip --output terraform.zip
 unzip terraform.zip
-cp terraform $HOME/.local/bin/terraform
+
+export PATH=${PATH}:$PWD
 
 cd ../terraform
 
