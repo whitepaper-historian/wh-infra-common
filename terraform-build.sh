3b9fbad955cd6fbd7d1cb921f999e8fc9536d82b
gcardonag <gcardonagarcia@gmail.com>
2018-02-18 13:53:42 -0500

    Making the lambci build script executable and updating the build script

--- terraform-build.sh
+++ terraform-build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-cd terraform
+mkdir tmp
+cd tmp
 #install terraform
+wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
+unzip terraform_0.11.3_linux_amd64.zip
+cp terraform $HOME/.local/bin/terraform
+
+cd ../terraform
+
 terraform init
 #plan
 #if changed, apply
