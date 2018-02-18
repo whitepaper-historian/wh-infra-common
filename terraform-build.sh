905d220d4440eb1b78730331cec968b306500c9a
gcardonag <gcardonagarcia@gmail.com>
2018-02-18 13:59:31 -0500

    Changing lambci build download package

--- terraform-build.sh
+++ terraform-build.sh
@@ -3,8 +3,8 @@
 mkdir tmp
 cd tmp
 #install terraform
-wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip
-unzip terraform_0.11.3_linux_amd64.zip
+curl https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip --output terraform.zip
+unzip terraform.zip
 cp terraform $HOME/.local/bin/terraform
 
 cd ../terraform
